fddddc607dc2a2de7c7d73cb5e4684e4b4c4600d
liuyajun <liuyajun52@gmail.com>
2014-05-27 02:01:42 +0800

    修改了一下用户头像间距

--- src/com/blacklighting/blwebio_android/MenuListFragment.java
+++ src/com/blacklighting/blwebio_android/MenuListFragment.java
@@ -9,7 +9,6 @@ import android.support.v4.app.ListFragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
 import android.widget.BaseAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -115,23 +114,23 @@ public class MenuListFragment extends ListFragment {
 		}
 	}
 
-	public class SampleAdapter extends ArrayAdapter<SampleItem> {
-
-		public SampleAdapter(Context context) {
-			super(context, 0);
-		}
-
-		public View getView(int position, View convertView, ViewGroup parent) {
-			if (convertView == null) {
-				convertView = LayoutInflater.from(getContext()).inflate(
-						R.layout.menu_row, null);
-			}
-			TextView title = (TextView) convertView
-					.findViewById(R.id.row_title);
-			title.setText(getItem(position).tag);
-
-			return convertView;
-		}
-
-	}
+//	public class SampleAdapter extends ArrayAdapter<SampleItem> {
+//
+//		public SampleAdapter(Context context) {
+//			super(context, 0);
+//		}
+//
+//		public View getView(int position, View convertView, ViewGroup parent) {
+//			if (convertView == null) {
+//				convertView = LayoutInflater.from(getContext()).inflate(
+//						R.layout.menu_row, null);
+//			}
+//			TextView title = (TextView) convertView
+//					.findViewById(R.id.row_title);
+//			title.setText(getItem(position).tag);
+//
+//			return convertView;
+//		}
+//
+//	}
 }
