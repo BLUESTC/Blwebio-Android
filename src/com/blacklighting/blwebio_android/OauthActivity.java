3296363cd138ad1552054ef683a0a62781ea2f67
liuyajun <liuyajun@localhost>
2014-08-04 16:01:30 +0800

    修复提交

--- src/com/blacklighting/blwebio_android/OauthActivity.java
+++ src/com/blacklighting/blwebio_android/OauthActivity.java
@@ -46,16 +46,16 @@ public class OauthActivity extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_oauth);
+//		setContentView(R.layout.activity_oauth);
 		new GetRequireTokenTask().execute();
 	}
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.oauth, menu);
-		return true;
-	}
+//	@Override
+//	public boolean onCreateOptionsMenu(Menu menu) {
+//		// Inflate the menu; this adds items to the action bar if it is present.
+//		getMenuInflater().inflate(R.menu.oauth, menu);
+//		return true;
+//	}
 
 	/**
 	 * 使用HmacSHA算法计算
